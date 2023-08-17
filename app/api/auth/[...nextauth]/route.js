e43f6de1cd904adea1542f2fff7934619483a492
Mason Cole <cole.mason2@gmail.com>
2023-08-17 14:32:11 -0500

    updates

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import User from "@models/user";
 
+import User from "@models/user";
 import { connectToDB } from "@utils/database";
 
 const handler = NextAuth({
@@ -11,38 +11,37 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    });
 
-    session.user.id = sessionUser._id.toString();
+  callbacks: {
+    async session({ session }) {
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
 
-    return session;
-  },
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
 
-      // check if a user already exists
-      const userExists = await User.findOne({
-        email: profile.email,
-      });
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
 
-      // if not, create a new user and save to db
-      if (!userExists) {
-        await User.create({
-          email: profile.eamil,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
-        });
+        // if not, create a new document and save user in MongoDB
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log("Error checking if user exists: ", error.message);
+        return false;
       }
-
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+    },
   },
 });
 
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -12,12 +12,12 @@ const Nav = () => {
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     };
-    setProviders();
+    setUpProviders();
   }, []);
 
   return (
@@ -48,7 +48,7 @@ const Nav = () => {
 
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -78,7 +78,7 @@ const Nav = () => {
         {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
